ec36a1a4b5df99f36ff36f515d5e4ad94606460d
Vincent Nguyen <vpvnguyen@gmail.com>
2019-08-23 20:04:16 -0700

    express server; require handlebars

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// MIDDLEWARE
+// static pages
+app.use(express.static('public'));
+
+// parse body as json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// import burgers routes and give the server access to them.
+var routes = require('./controllers/burgers_controller.js');
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // log (server-side) when server has started
+    console.log('Server listening on: http://localhost:' + PORT);
+});
