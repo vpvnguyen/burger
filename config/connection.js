0f05724e08724599c29f4e5d014439592ffd096c
Vincent Nguyen <vpvnguyen@gmail.com>
2019-08-25 19:57:06 -0700

    import and connect to mysql

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+
+// mysql config
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8889,
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+});
+
+// connect to mysql
+connection.connect((err) => {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+// export connection for ORM
+module.exports = connection;
