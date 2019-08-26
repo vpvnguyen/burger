dca0c31b23b47004de4fcfc042159154eda848c2
Vincent Nguyen <vpvnguyen@gmail.com>
2019-08-25 20:01:39 -0700

    get, post, put routing; server-side logging when routes are hit

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,49 @@
+
+var express = require('express');
+var router = express.Router();
+
+// import the burgers.js to use db functions for burgers
+var burger = require('../models/burger.js');
+
+// routing
+router.get('/', (req, res) => {
+    console.log('\nGET /');
+
+    burger.selectAll((data) => {
+        var burgersObject = {
+            burgers: data
+        };
+        res.render('index', burgersObject);
+    });
+});
+
+// add burger with default condition of devoured = false
+router.post('/burgers', (req, res) => {
+    console.log('\nPOST /burgers');
+
+    burger.insertOne([
+        'burger_name', 'devoured'
+    ], [
+            req.body.burger_name, false
+        ], (data) => {
+            res.redirect('/');
+        });
+});
+
+// update burger devoured state
+router.put('/burgers/:id', (req, res) => {
+    var id = req.params.id;
+    console.log(`\nPUT /burgers/:${id}`);
+
+    burger.updateOne('devoured', true, id, (data) => {
+        if (data.changedRows == 0) {
+            // if no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
